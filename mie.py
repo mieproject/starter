bf4b1858ecf70df2987fc7e53aff09d9630f1b24
AnsKal <anskal@localhost.localdomain>
2021-06-20 15:35:44 +0200

    more stable

--- mie.py
+++ mie.py
@@ -1,58 +1,27 @@
 #!/usr/bin/python3
+# coding: utf-8
 import argparse, sys, re, requests
-from datetime import datetime
 from colorama import init
 from termcolor import colored
-
+# use Colorama to make init() Termcolor work on Windows too
+init()
+import mie_helpers
 # to run BASH cmds
 import subprocess
 
 
-# use Colorama to make init() Termcolor work on Windows too
-init()
 
 parser=argparse.ArgumentParser()
 
 parser.add_argument('--project-name', '-n', help="project name", required=True)
+parser.add_argument('--project-path', help="project path", required=False,default='~/mieprojects')
 parser.add_argument('--type', '-t', help="project type [web,api has also web]", type= str,default="web")
-parser.add_argument('--auth', help="'y' if you need auth in ur project [y|n]", type= str,default="y")
-parser.add_argument('--run-npm', help="'y' if you need to run `npm install && npm run dev` in ur project [y|n]", type= str,default="y")
+parser.add_argument('--auth', help="'y' if you need auth in ur project [y|n]",choices=['y', 'n'], type= str,default="y")
+parser.add_argument('--run-npm', help="'y' if you need to run `npm install && npm run dev` in ur project [y|n]",choices=['y', 'n'], type= str,default="y")
 # parser.add_argument('--packages','-p', help="all wanted packages separated by comma,", type= str)
 parser.add_argument('--modules','-m', help="all wanted modules separated by comma,", type= str,default="")
 args=parser.parse_args()
 
-
-
-
-#helpers
-def string_time():
-  now = datetime.now()
-  dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-  return dt_string
-
-def _info(s):
-  print(colored('\r\n'+s+'\r','white','on_blue'))
-def _err(s):
-  print(colored('\r\n'+s+'\r','white','on_red'))
-def make_safe_filename(s):
-    def safe_char(c):
-        if c.isalnum() or c=='.':
-            return c
-        else:
-            return "_"
-
-    safe = ""
-    last_safe=False
-    for c in s:
-      if len(safe) > 200:
-        return safe + "_" + str(time.time_ns() // 1000000)
-
-      safe_c = safe_char(c)
-      curr_safe = c != safe_c
-      if not last_safe or not curr_safe:
-        safe += safe_c
-      last_safe=curr_safe
-    return safe   
 
 
 #init 
@@ -61,10 +30,10 @@ def make_safe_filename(s):
 
     
 # project init
-projects_path = '~/mieprojects'
+projects_path = args.project_path
 
 pname = args.project_name
-pfolder = projects_path+'/'+make_safe_filename(pname)
+pfolder = projects_path+'/'+mie_helpers.make_safe_filename(pname)
 # pfolder = (pname)
 # cd to project 
 precmd = "cd "+pfolder+" && "
@@ -77,10 +46,10 @@ def main():
     # start
     
     
-    _info('['+string_time()+'] create new laravel project')
+    mie_helpers._info('['+mie_helpers.string_time()+'] create new laravel project')
     subprocess.call('composer create-project laravel/laravel '+pfolder+'', shell=True)
     subprocess.call(precmd+"composer install ", shell=True)
-    _info('start `install_packages`')
+    mie_helpers._info('start `install_packages`')
     install_packages()
     
 
@@ -100,11 +69,11 @@ def install_packages():
   if(args.modules != ''):
     modules_names = (args.modules).split(",")
     for module_name in modules_names:
-      _info('['+string_time()+'] install module:'+ module_name)
+      mie_helpers._info('['+mie_helpers.string_time()+'] install module:'+ module_name)
       if(requests.get("https://packagist.org/packages/mieproject/"+module_name, allow_redirects=False).status_code == 200):
         subprocess.call(precmd+"composer require mieproject/"+ module_name, shell=True)
       else:
-          _err('module "'+module_name+'" not exist')
+          mie_helpers._err('module "'+module_name+'" not exist')
           
 
   # packages_names = (args.packages).split(",")
--- mie_helpers.py
+++ mie_helpers.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from termcolor import colored
+
+
 def make_safe_filename(s):
     def safe_char(c):
         if c.isalnum() or c=='.':
@@ -17,3 +21,13 @@ def make_safe_filename(s):
         safe += safe_c
       last_safe=curr_safe
     return safe   
+
+def string_time():
+  now = datetime.now()
+  dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+  return dt_string
+
+def _info(s):
+  print(colored('\r\n'+s+'\r','white','on_blue'))
+def _err(s):
+  print(colored('\r\n'+s+'\r','white','on_red'))
